5459d5029772406e370d26686e09efb26647113a
wcxaaa <wcxaaa@live.com>
2017-09-20 11:07:10 +0800

    + add delete dist folder functionality  * also ingores dist folder

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -52,6 +52,7 @@ module.exports = {
   },
 
   plugins: [
+
     // Workaround for angular/angular#11580
     new webpack.ContextReplacementPlugin(
       // The (\\|\/) piece accounts for path separators in *nix and Windows
